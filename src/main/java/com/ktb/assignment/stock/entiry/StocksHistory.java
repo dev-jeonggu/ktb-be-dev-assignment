3373bf4eee34f9a547c5f69463abde09598466df
jeonggu <dev.jeonggu@gmail.com>
2025-02-15 04:18:15 +0900

    feat: add entity and remove outdated Stock entity
    - add Company entity
    - add StocksHistory entity
    - add StocksHistoryId for composite key
    - remove Stock entity (replaced by StocksHistory)

---
+++ src/main/java/com/ktb/assignment/stock/entiry/StocksHistory.java
@@ -0,0 +1,35 @@
+package com.ktb.assignment.stock.entiry;
+
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "stocks_history")
+public class StocksHistory {
+
+    @EmbeddedId
+    private StocksHistoryId id;
+
+    @ManyToOne
+    @MapsId("companyCode")  // 복합키의 companyCode를 Company 엔티티와 매핑
+    @JoinColumn(name = "company_code", referencedColumnName = "company_code")
+    private Company company;
+
+    @Column(name = "open_price", nullable = false)
+    private float openPrice;
+
+    @Column(name = "high_price", nullable = false)
+    private float highPrice;
+
+    @Column(name = "low_price", nullable = false)
+    private float lowPrice;
+
+    @Column(name = "close_price", nullable = false)
+    private float closePrice;
+
+    @Column(name = "volume", nullable = false)
+    private float volume;
+}
